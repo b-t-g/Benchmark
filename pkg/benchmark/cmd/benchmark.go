8d78fc5a914568bcff1fcb55fb46e4fb6560bc57
Brendan <brendan@example.com>
2022-10-23 17:03:30 -0700

    Check if query-params flag exists and is valid.

--- pkg/benchmark/cmd/benchmark.go
+++ pkg/benchmark/cmd/benchmark.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/b-t-g/benchmark/pkg/benchmark/query"
@@ -17,6 +18,17 @@ const (
 )
 
 func benchmark(cmd *cobra.Command, args []string) {
+	if QueryParamsPath == "" {
+		fmt.Println("Missing required field query-params")
+		os.Exit(1)
+	}
+	file, err := os.Open(QueryParamsPath)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
 	wg := new(sync.WaitGroup)
 	for i := 0; i < NumWorkers; i++ {
 		wg.Add(1)

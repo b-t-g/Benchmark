f52e1e911a1a8830dcf659525f8ff73ff5de3a3d
Brendan <brendan@example.com>
2022-10-25 13:51:36 -0700

    Add explanatory comments for calculating statistics.

--- pkg/benchmark/statistics/statistics.go
+++ pkg/benchmark/statistics/statistics.go
@@ -34,6 +34,9 @@ func ProcessQueryStatistics(stats Statistics) ProcessedStatistics {
 	return r
 }
 
+// Include standard deviation to be able to better interpret the other statistics,
+// especially min and max. E.g., determine whether min and max are outliers or if
+// most of the queries are just very spread out.
 func standardDeviation(durations []int64, avg float64) float64 {
 	var sd float64
 	for _, i := range durations {
@@ -52,6 +55,17 @@ func average(durations []int64) float64 {
 	return float64(avg)/float64(len(durations))
 }
 
+// This is the simplest approach, and hence easiest to understand/change, but, in terms of time complexity, is not optimal.
+// If improving statistics calculation time could lead to an appreciably better experience, we have a few options:
+//
+// 1. Use a better algorithm, namely the "quickselect" algorithm with the (expected) complexity
+//    of O(N) where N is the length of the array.
+//
+// 2. Keep the algorithms for calculating statistics largely the same, but amortize their calculation. I.e.,
+//    when sorting the array (e.g., using heapsort, which analyzes each element individually by "removing" each element
+//    from the heap), we could calculate the average at the same time. This would not lead to a runtime improvement in terms
+//    of computational complexity, but could be an appreciable improvement to a user when there are a sufficiently large number
+//    of queries.
 func median(durations []int64) float64 {
 	sort.Slice(durations, func(i,j int) bool {
 		return durations[i] < durations[j]

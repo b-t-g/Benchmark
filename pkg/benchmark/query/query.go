386bdc8af229c1d42b42118a4b6256563ed08858
Brendan <brendan@example.com>
2022-10-25 14:57:56 -0700

    Calculate statistics for multiple queries in a thread.

--- pkg/benchmark/query/query.go
+++ pkg/benchmark/query/query.go
@@ -19,7 +19,7 @@ const (
 	sslmode  = "disable"
 )
 
-func RunQuery(query string) statistics.Statistics {
+func RunQuery(queries []string) statistics.Statistics {
 	hostname := os.Getenv("DB_HOSTNAME")
 	if hostname == "" {
 		log.Fatal("DB_HOSTNAME for declaring the DB hostname is empty!")
@@ -41,23 +41,25 @@ func RunQuery(query string) statistics.Statistics {
 	defer conn.Close(ctx)
 	stats := statistics.Statistics{}
 
-	queryStart := time.Now()
-	_, err = conn.Query(ctx, query)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-	queryEnd := time.Now()
-	queryDuration := queryEnd.Sub(queryStart).Milliseconds()
+	for _, query := range queries {
+		queryStart := time.Now()
+		_, err = conn.Query(ctx, query)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+		queryEnd := time.Now()
+		queryDuration := queryEnd.Sub(queryStart).Milliseconds()
 
-	stats.Durations = append(stats.Durations, queryDuration)
-	if queryDuration < stats.Min.QueryMsDuration || stats.Min.QueryMsDuration == 0 {
-		stats.Min = statistics.QueryStatistic{QueryMsDuration: queryDuration, Query: query}
-	}
+		stats.Durations = append(stats.Durations, queryDuration)
+		if queryDuration < stats.Min.QueryMsDuration || stats.Min.QueryMsDuration == 0 {
+			stats.Min = statistics.QueryStatistic{QueryMsDuration: queryDuration, Query: query}
+		}
 
-	if queryDuration > stats.Max.QueryMsDuration {
-		stats.Max = statistics.QueryStatistic{QueryMsDuration: queryDuration, Query: query}
-	}
+		if queryDuration > stats.Max.QueryMsDuration {
+			stats.Max = statistics.QueryStatistic{QueryMsDuration: queryDuration, Query: query}
+		}
 
+	}
 	return stats
 }

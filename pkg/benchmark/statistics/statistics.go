94efdf3cfa90ff764c864bfc12e7694ef2c4780c
Brendan <brendan@example.com>
2022-10-25 11:29:14 -0700

    Define struct for processed output

--- pkg/benchmark/statistics/statistics.go
+++ pkg/benchmark/statistics/statistics.go
@@ -12,3 +12,11 @@ type QueryStatistic struct {
 	QueryMsDuration int64
 	Query           string
 }
+
+type ProcessedStatistics struct {
+	Min     QueryStatistic
+	Max     QueryStatistic
+	Median  float64
+	Average float64
+	StdDev  float64
+}

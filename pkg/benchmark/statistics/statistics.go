74965d6268c16e39cd7ef2236fdb81f700b13581
Brendan <brendan@example.com>
2022-10-25 13:34:26 -0700

    Add functions for computing output statistics.

--- pkg/benchmark/statistics/statistics.go
+++ pkg/benchmark/statistics/statistics.go
@@ -1,6 +1,9 @@
 package statistics
 
-import "time"
+import (
+	"math"
+	"sort"
+)
 
 type Statistics struct {
 	Min       QueryStatistic
@@ -20,3 +23,44 @@ type ProcessedStatistics struct {
 	Average float64
 	StdDev  float64
 }
+
+func ProcessQueryStatistics(stats Statistics) ProcessedStatistics {
+	r := ProcessedStatistics{}
+	r.Median = median(stats.Durations)
+	r.Average = average(stats.Durations)
+	r.StdDev = standardDeviation(stats.Durations, r.Average)
+	r.Min = stats.Min
+	r.Max = stats.Max
+	return r
+}
+
+func standardDeviation(durations []int64, avg float64) float64 {
+	var sd float64
+	for _, i := range durations {
+		sd += math.Pow(float64(i) - avg, 2)
+	}
+
+	return math.Sqrt(sd/(float64(len(durations))))
+}
+
+func average(durations []int64) float64 {
+	avg := int64(0)
+	for _, i := range durations {
+		avg += i
+	}
+
+	return float64(avg)/float64(len(durations))
+}
+
+func median(durations []int64) float64 {
+	sort.Slice(durations, func(i,j int) bool {
+		return durations[i] < durations[j]
+	})
+
+	if len(durations) % 2 == 1 {
+		return float64(durations[(len(durations) - 1)/2])
+	}
+
+	return (float64(durations[(len(durations)/2) - 1]) + float64(durations[(len(durations)/2)]))/2
+}
+

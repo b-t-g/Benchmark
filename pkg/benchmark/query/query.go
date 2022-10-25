583dee23c0588fa9da66f4d195765d7a4a0e87bd
Brendan <brendan@example.com>
2022-10-25 16:34:33 -0700

    Use connection pool in query package.

--- pkg/benchmark/query/query.go
+++ pkg/benchmark/query/query.go
@@ -8,48 +8,32 @@ import (
 	"time"
 
 	"github.com/b-t-g/benchmark/pkg/benchmark/statistics"
-	pgx "github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-const (
-	username = "postgres"
-	password = "example"
-	port     = 5432
-	database = "homework"
-	sslmode  = "disable"
-)
-
-func RunQuery(queries []string) statistics.Statistics {
-	hostname := os.Getenv("DB_HOSTNAME")
-	if hostname == "" {
-		log.Fatal("DB_HOSTNAME for declaring the DB hostname is empty!")
-	}
-
-	// In a Kubernetes environment, instead of hard-coding, I'd create a Kubernetes
-	// secret and, in both the stateful set for the database (if it's deployed myself)
-	// and the deployment for this benchmark tool, I'd use the secret as an environment
-	// variable as shown here:
-	// https://kubernetes.io/docs/concepts/configuration/secret/#using-secrets-as-environment-variables
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
-		username, password, hostname, port, database, sslmode)
-	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, connStr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(ctx)
+func RunQuery(queries []string, pool *pgxpool.Pool) statistics.Statistics {
 	stats := statistics.Statistics{}
 
 	for _, query := range queries {
+		ctx, cFunc := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cFunc()
+
+		conn, err := pool.Acquire(ctx)
+		if err != nil {
+			log.Fatalf("Error acquiring connection from pool: %v", err)
+		}
+
 		queryStart := time.Now()
 		_, err = conn.Query(ctx, query)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
 			os.Exit(1)
 		}
+
 		queryEnd := time.Now()
 		queryDuration := queryEnd.Sub(queryStart).Milliseconds()
+
+		conn.Release()
 
 		stats.Durations = append(stats.Durations, queryDuration)
 		if queryDuration < stats.Min.QueryMsDuration || stats.Min.QueryMsDuration == 0 {
